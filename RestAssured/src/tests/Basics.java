79d9e1543a1d06ce334231d3f33e2954dbdff94d
ilhammuksidin <ilham.muksidin@dana.id>
2022-03-21 00:59:19 +0700

    Update file basics.java

--- RestAssured/src/tests/Basics.java
+++ RestAssured/src/tests/Basics.java
@@ -15,23 +15,36 @@ public class Basics {
 
 	public static void main(String[] args) {
 		
+		// json
 		payload payload = new payload();
-		String order_status = "";
-		String last_updated_timestamp = "";
-		String uri = "/";
 		
-		RestAssured.baseURI = "";
+		// init parameter
+		String order_status = "IN PROGRESS";
+		String last_updated_timestamp = "16423212104390000000";
+		String uri = "/order";
+		
+		// base endpoint
+		RestAssured.baseURI = "https://6237655af5f6e28a154c1af1.mockapi.io";
+		
+		// gherkin
 		String response = given().header("Content-Type", "application/json").
+		// for test fill the value of parameter order_status and last_updated_timestamp
 		body(payload.processOrder(order_status,last_updated_timestamp))
 		.when()
 		.post(uri)
-		.then().assertThat().statusCode(200)
+		// assertion
+		.then().assertThat().statusCode(201)
 		.extract().response().asString();
 		
 		CommonMethods x = new CommonMethods();
 		JsonPath js = x.rawToJson(response);
 		String outcomeOrderStatus = js.get("order_status");
-		System.out.println(outcomeOrderStatus);
+		String outcomeLastUpdatedTimestamp = js.get("last_updated_timestamp");
+		
+		String newLine = System.getProperty("line.separator");
+		System.out.println("API Response Body:" +response + newLine);
+		System.out.println(outcomeOrderStatus + newLine);
+		System.out.println(outcomeLastUpdatedTimestamp + newLine);
 	
 	}
 
